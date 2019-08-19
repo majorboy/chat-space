475c47141fd0b66c3f6d070ded7020f7bbda0b33
KengoNagata <majorboy47@gmail.com>
2019-08-19 19:07:25 +0900

    Step2-4  ajax_message#create

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var message = new FormData(this); 
+    var url = window.location.href + '/messages'
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: message,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+  })
 });
