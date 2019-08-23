55d661ab62ba8b5782ffd89351d96d7a1ea55816
KengoNagata <majorboy47@gmail.com>
2019-08-24 07:43:06 +0900

    チャットグループ　メンバー表示修正
    Edit画面時において現在のチャットグループメンバーが表示されるようにした

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -63,10 +63,9 @@ $(function(){
       $(document).on('click','.user-search-add',function(){
         var name = $(this).attr("data-user-name");
         var user_id = $(this).attr("data-user-id");
-        appendUserToMemberList(name, user_id);
         $(this).parent().remove();
+        appendUserToMemberList(name, user_id);
       });
-
       $(document).on("click",'.user-search-remove', function(){
         $(this).parent().remove();
       });
