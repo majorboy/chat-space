09ac3e2887e544061ace29ff483318f5ebbe3930
KengoNagata <majorboy47@gmail.com>
2019-08-24 07:51:34 +0900

    チャットメンバー削除機能の不具合修正
    不具合：削除ボタンがインクリメンタルサーチのコード内に入っているため、Keyup後にしか削除ボタンが機能しない
    
    修正内容：削除ボタンのコードをインクリメンタルサーチ用コードのブロック外に移動。いつでも削除ボタンが動作するようにした

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -58,17 +58,16 @@ $(function(){
     .fail(function(){
       alert('ユーザー検索に失敗しました');
     });
-
-    $(function(){
-      $(document).on('click','.user-search-add',function(){
-        var name = $(this).attr("data-user-name");
-        var user_id = $(this).attr("data-user-id");
-        $(this).parent().remove();
-        appendUserToMemberList(name, user_id);
-      });
-      $(document).on("click",'.user-search-remove', function(){
-        $(this).parent().remove();
-      });
+  });
+  $(function(){
+    $(document).on('click','.user-search-add',function(){
+      var name = $(this).attr("data-user-name");
+      var user_id = $(this).attr("data-user-id");
+      $(this).parent().remove();
+      appendUserToMemberList(name, user_id);
+    });
+    $(document).on("click",'.user-search-remove', function(){
+      $(this).parent().remove();
     });
   });
 });
