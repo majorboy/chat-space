dd81a74212fefc858fac3ae3ca6c4940190708cf
KengoNagata <majorboy47@gmail.com>
2019-08-26 21:38:01 +0900

    不具合修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'
+set :deploy_to, "/var/www2/rails/chat-space"
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
